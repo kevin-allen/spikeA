ce71f705a9d3de5e54b3b4dc8c0ed254730b94de
Kevin Allen <allen@uni-heidelberg.de>
2021-05-01 21:16:15 +0200

    download with gdown

--- spikeA/Downloads.py
+++ spikeA/Downloads.py
@@ -2,6 +2,7 @@ import requests
 import os.path
 import tarfile
 import tempfile 
+import gdown
 
 
 def download_file_from_google_drive(id, destination):
@@ -44,14 +45,21 @@ def download_spikeA_data_one_session(destination=None):
         print('Created temporary directory', tmpdirname)
         tmp_destination = tmpdirname+"/"+"mn1385-16032021-0101.tar.gz"
         print("Downloading compressed file to",tmp_destination)
-        download_file_from_google_drive(file_id, tmp_destination)
+        
+        # This uses requests to download the file
+        #download_file_from_google_drive(file_id, tmp_destination)
+        
+        # This uses gdown, better error message but is an additional requirement.
+        url = 'https://drive.google.com/uc?id=1xq3wx-k8hv7oLKQqcjoiXxn7aWhwS_6B'
+        gdown.download(url, tmp_destination, quiet=False)
         
         # get file size
         f_size = os.path.getsize(tmp_destination)
         print("Size of downloaded file: {}".format(f_size))
         expected_size = 2412591360
         if(f_size < 2412591360):
-            print("Size of downloaded file is too small, should be 2412591360. Problem with the download")
+            print("Problem downloading the file")
+            print("It might be that too many people tried to download it in a short time period.")
             print("You can try to download it with this link:")
             print("https://drive.google.com/file/d/1xq3wx-k8hv7oLKQqcjoiXxn7aWhwS_6B/view?usp=sharing")
             raise OSError("Problem with downloading the data file.")
