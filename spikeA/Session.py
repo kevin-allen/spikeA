37c318564d525bc289104f1cff5f5fd64f414ea9
pbozac <ritz6513@gmail.com>
2021-05-04 16:42:38 +0200

    change the returned format of tetrode_channels from string to int

--- spikeA/Session.py
+++ spikeA/Session.py
@@ -82,7 +82,7 @@ class TetrodeSession(Session):
         self.n_tetrodes = int(df[2].split(' ')[0])
         # create a list of 1D array with the channels for each tetrode
         tmp = df[3:int(self.n_tetrodes)+3]
-        self.tetrode_channels = [tmp[i].split(' ')[1:] for i in range(0, len(tmp))]
+        self.tetrode_channels = [list(map(int, tmp[i].split(' ')))[1:] for i in range(0, len(tmp))]
         # read the number of trials
         self.n_trials = int(df[int(self.n_tetrodes)+3])
         # get a list of trial names
