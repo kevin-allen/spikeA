c1b98069cf7a85c2b8e4568e477fc8b1f99e8d00
Kevin Allen <allen@uni-heidelberg.de>
2022-12-03 21:55:02 +0100

    add class to simulate grid cells spike trains

--- spikeA/Neuron.py
+++ spikeA/Neuron.py
@@ -194,4 +194,132 @@ class Simulated_place_cell(Neuron):
         
         self.ap.pose[:,0] = np.arange(start=tStepSize,stop = maxT,step=tStepSize)
         self.ap.pose_ori = self.ap.pose
+
+        
+class Simulated_grid_cell(Neuron):
+    """
+    Class to simulate the spike train of a grid cell.
+    
+    The firing rate in 2D space is simulated with cos function
+    
+    Arguments
+    name: name of the simulated grid cell
+    offset: np.array of shape 2,1 or 2,
+    orientation: in radian
+    spacing: distance between closest firing fields
+    peak_rate: firing field peak firing rate in Hz
+    
+    sampling_rate: sampling rate of the spike train
+    ap: Animal_pose object used to build the spike train
+    
+    """
+    def __init__(self, name, 
+                 offset=np.array([[0],[0]]),
+                 orientation = 0.0,
+                 spacing = 30,
+                 peak_rate=20, 
+                 sampling_rate=20000, 
+                 ap=None):
+        super(Simulated_grid_cell,self).__init__(name=name)
+    
+        # variable defining a grid cell
+        if offset.ndim ==1:
+            offset = np.expand_dims(offset,1)
+        
+        
+        self.offset=offset
+        self.orientation=orientation
+        self.spacing = spacing
+        self.peak_rate = peak_rate
+        
+        
+        self.sampling_rate = sampling_rate      
+        self.ap = ap
+        self.spike_train = Spike_train(name=self.name,sampling_rate=self.sampling_rate)
+        
+        self.remove_nan_from_ap()
+        self.simulate_spike_train()
+        
+        self.spatial_properties = Spatial_properties(animal_pose=self.ap,spike_train=self.spike_train)
+        
+        self.spike_train.set_intervals(self.inter)
+        self.ap.set_intervals(self.inter)
+        
+
+    def simulate_spike_train(self):
+        """
+        Get a spike train that is based on the spatial selectivity of the neuron
+        """
+        
+        # we get the rate for all position
+        poses = np.stack([self.ap.pose[:,1],self.ap.pose[:,2]]).T
+        
+        deltas = [self.orientation, self.orientation+np.pi/3, self.orientation+np.pi/3*2]
+        
+        rates = np.apply_along_axis(self.grid_firing_rate_at_p,1,
+                                   poses, self.offset,deltas,self.spacing,self.peak_rate)
+        
+        # higher sampling rate
+        newTime = np.arange(start=self.ap.pose[0,0], stop = self.inter[0,1]-1,step=1/self.sampling_rate)
+        
+        # to interpolate the rate at a higher sampling rate
+        fx = interp1d(self.ap.pose[:,0], rates) 
+        
+
+        mu = fx(newTime) # interpolate the rate 
+        
+        self.spike_train.generate_poisson_spike_train_from_rate_vector(mu ,sampling_rate=self.sampling_rate)
+    
+    
+    def grid_firing_rate_at_p(self,pose,offset,deltas,spacing,peak_rate):
+        """
+        Function to calculate the firing rate of a grid cell at any 2D position
+
+        Arguments
+        pose: np.array of shape (2,1)
+        offset: np.array of shape (2,1)
+
+        """
+        if pose.ndim == 1:
+            pose = np.expand_dims(pose,1)
+    
+        p = pose+offset
+
+        # we want to get the x value of the position vector, after rotating the position vector by different amount 
+        # these are like rotation matrices, but we remove the terms that would give us the y component. 
+        Rx0 = np.array([[np.cos(deltas[0]),-np.sin(deltas[0])]])
+        Rx1 = np.array([[np.cos(deltas[1]),-np.sin(deltas[1])]])
+        Rx2 = np.array([[np.cos(deltas[2]),-np.sin(deltas[2])]])
+
+        c0 = np.cos(Rx0@p * (np.pi*2) / spacing) # we rotate the position vector and only get the x value of the rotated vector, than we normalize so that there is one cycle per spacing, cos function gives the oscillation
+        c1 = np.cos(Rx1@p * (np.pi*2) / spacing)
+        c2 = np.cos(Rx2@p * (np.pi*2) / spacing)
+
+
+        return (((c0+c1+c2)+1.5)/(3.0+1.5) * peak_rate)[0,0] # we sum the 3 components,  normalize so that the range is from 0 to 1, then multiply by the value that we want as peak rate
+
+    
+    
+    
+    def remove_nan_from_ap(self):
+        """
+        Remove the nan from the ap.pose.
+        Only x,y and hd values are considered
+        
+        The ap object will be permenantly modified.
+        If ap does not have np.nan value, this should not have any effect.
+        """
+        
+        
+        tStepSize = self.ap.pose[1,0]-self.ap.pose[0,0]
+        pose = np.stack([self.ap.pose[:,0],self.ap.pose[:,1],self.ap.pose[:,2],self.ap.pose[:,4]]).T # only consider the data that we will be using
+        keepIndices = ~np.isnan(pose).any(axis=1)
+        maxT = np.sum(keepIndices)*tStepSize+tStepSize
+        
+        self.ap.pose = self.ap.pose[keepIndices]
+        
+        self.inter = np.array([[0,maxT]])
+        
+        self.ap.pose[:,0] = np.arange(start=tStepSize,stop = maxT,step=tStepSize)
+        self.ap.pose_ori = self.ap.pose
         
